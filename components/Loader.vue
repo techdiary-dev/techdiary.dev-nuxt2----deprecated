4e2c1bab6d15d644fb8c1d0f294060cad0f7a033
KingRayhan <rayhan095@gmail.com>
2021-07-24 03:13:02 +0600

    homepage infinite scrolling

---
+++ components/Loader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="my-5 text-center">
+    <div class="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 60px;
+  height: 60px;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid #000;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,23 +8,85 @@
         :article="article"
       />
     </div>
+    <Loader
+      v-observe-visibility="loadMore"
+      v-if="this.pageMeta.current_page != this.pageMeta.last_page"
+    />
   </div>
 </template>
 
 <script>
 export default {
   layout: "three-columns",
-  data() {
+  head() {
     return {
-      articles: []
+      title: "নীড়",
+      meta: [
+        {
+          name: "description",
+          content: `টেকডায়েরি | বাংলার প্রোগ্রামিং নেটওয়ার্ক`
+        },
+        {
+          property: "og:title",
+          content: `টেকডায়েরি | বাংলার প্রোগ্রামিং নেটওয়ার্ক`
+        },
+        {
+          property: "og:url",
+          content: "https://www.techdiary.dev"
+        },
+        {
+          property: "og:description",
+          content: `টেকডায়েরি | বাংলার প্রোগ্রামিং নেটওয়ার্ক`
+        },
+        {
+          property: "og:image",
+          content:
+            "https://res.cloudinary.com/techdiary-dev/image/fetch/c_scale,f_auto,q_auto,w_1200/https://user-images.githubusercontent.com/7611746/82744130-38b0fd80-9d96-11ea-8223-62d62a56566f.png"
+        },
+        {
+          property: "og:image:width",
+          content: "1200"
+        },
+        {
+          property: "og:image:height",
+          content: "630"
+        }
+      ]
     };
   },
-  async mounted() {
-    const { data } = await this.$axios.$get("api/articles");
-
-    this.articles = data;
+  data() {
+    return {
+      articles: [],
+      pageMeta: {
+        current_page: 1,
+        last_page: null
+      }
+    };
   },
-
-  async fetch() {}
+  async fetch() {
+    try {
+      const {
+        data,
+        meta: { current_page, last_page }
+      } = await this.$axios.$get(
+        `/api/articles?page=${this.pageMeta.current_page}`
+      );
+      this.articles = this.articles.concat(data);
+      this.pageMeta = { current_page, last_page };
+    } catch (error) {
+      console.log({ error });
+    }
+  },
+  methods: {
+    async loadMore(isVisible) {
+      if (isVisible) {
+        if (this.pageMeta.current_page >= this.pageMeta.last_page) {
+          return;
+        }
+        this.pageMeta.current_page++;
+        await this.$fetch();
+      }
+    }
+  }
 };
 </script>
