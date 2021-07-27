d2c3e2807071e9293cd4792d9c76335fcb3241b4
KingRayhan <rayhan095@gmail.com>
2021-07-27 17:26:06 +0600

    wip:dashboard

---
+++ layouts/dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="dashboard-layout wrapper">
+      <aside class="dashboard-layout__sidebar">
+        <h1>Sidebar</h1>
+      </aside>
+      <main class="dashboard-layout__main">
+        <Nuxt />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dashboard-layout {
+  @apply flex min-h-screen;
+
+  &__sidebar {
+    @apply w-52 flex-none p-2;
+    @apply border-r border-gray-200 dark:border-gray-700;
+  }
+
+  &__main {
+    @apply flex-auto;
+  }
+}
+</style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+<script>
+export default {
+  middleware: ["auth"],
+  layout: "dashboard"
+};
+</script>
