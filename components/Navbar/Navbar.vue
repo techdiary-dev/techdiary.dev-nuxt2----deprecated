f308c2b4e03dfc7b8771761be4dc55965d7c6c4e
kingrayhan <rayhan095@gmail.com>
2021-10-01 13:22:20 +0600

    sorry batch

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,41 +1,43 @@
 <template>
-  <div>
+  <div class="navbar app-border-color">
+    <div class="navbar__inner wrapper">
+      <div class="navbar__left">
+        <button class="navbar__hambargur-button" @click="triggerSidebar(true)">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="navbar__hambargur-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        <nuxt-link to="/" class="inline-block ">
+          <Logos-Text class="navbar__logo" />
+        </nuxt-link>
+      </div>
+      <Navbar-Links />
+    </div>
     <div
-      class="flex items-center justify-center p-4 bg-red-200 border-red-500 rounded-md "
+      class="flex items-center justify-center p-2 bg-yellow-300 border-red-500 "
     >
       <p class="text-gray-600 ">
-        আমরা নতুন UI এবং ফিচার নিয়ে কাজ করছি, তাই লগইন এর মত কিছু ফিচার বর্তমান
-        কাজ নাও করতে পারে। এই অসুবিধার জন্য অত্যন্ত দুঃখিত
+        আমরা নতুন UI এবং ফিচার নিয়ে কাজ করছি, তাই কিছু ফিচার বর্তমানে কাজ নাও
+        করতে পারে। এই অসুবিধার জন্য অত্যন্ত দুঃখিত। কোন বাগ পেলে আমাদের জানিয়ে
+        দিতে পারেন
+        <a
+          target="_blank"
+          href="https://github.com/techdiary-dev/feedback/issues/new/choose"
+          >ইস্যু তৈরি</a
+        >
+        করে জানি দিন।
       </p>
-    </div>
-    <div class="navbar app-border-color">
-      <div class="navbar__inner wrapper">
-        <div class="navbar__left">
-          <button
-            class="navbar__hambargur-button"
-            @click="triggerSidebar(true)"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="navbar__hambargur-icon"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-          <nuxt-link to="/" class="inline-block ">
-            <Logos-Text class="navbar__logo" />
-          </nuxt-link>
-        </div>
-        <Navbar-Links />
-      </div>
     </div>
   </div>
 </template>
