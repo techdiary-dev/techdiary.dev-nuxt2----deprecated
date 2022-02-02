77b37e4d41e5b6904259fec6775ab85c06acd962
kingrayhan <rayhan095@gmail.com>
2022-02-02 16:56:38 +0600

    remove resizeable-textarea package

--- plugins/resizeable-input.js
+++ plugins/resizeable-input.js
@@ -1,6 +1,3 @@
 import Vue from "vue";
 import VueInputAutowidth from "vue-input-autowidth";
-import TextareaAutosize from "vue-textarea-autosize";
-
 Vue.use(VueInputAutowidth);
-Vue.use(TextareaAutosize);
