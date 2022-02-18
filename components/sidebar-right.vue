f7660bd70c4b0d5bdfee4f05d696a425e7e8a1f6
King Rayhan <rayhan095@gmail.com>
2022-02-18 12:15:15 +0600

    Update sidebar-right.vue

--- components/sidebar-right.vue
+++ components/sidebar-right.vue
@@ -4,8 +4,8 @@
         <!-- <widgets-top-writters /> -->
         <widgets-social-links />
         <widgets-links />
-        <widgets-vuejs-bootcamp />
-        <!-- <widgets-discord /> -->
+        <!-- <widgets-vuejs-bootcamp /> -->
+        <widgets-discord />
         <widgets-sponsor />
         <widgets-latest-registered-users />
     </div>
